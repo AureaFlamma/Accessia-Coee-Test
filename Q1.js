e0022eca5122b0dba658778408932e0e4a8cc533
Domus Aurea <philip.doge.kaminski@gmail.com>
2023-01-12 17:16:48 +0000

    about to change for loop into a for-of loop

--- Q1.js
+++ Q1.js
@@ -1,31 +1,25 @@
 function countUnpaired(input) {
   let matches = [];
-
-  for (let i = 0; i < input.length; i++) {
-    for (let k = 0; k < input.length; k++) {
-      if (input[i] === input[k] && i !== k) {
-        console.log("equal value, different indices: ", [input[i], input[k]]);
-      } else if (input[i] === input[k] && i === k) {
-        console.log("equal value, same indices:", [input[i], input[k]]);
-      } else if (input[i] !== input[k] && i !== k) {
-        console.log("different value, different indices: ", [
-          input[i],
-          input[k],
-        ]);
-      } else {
-        console.log("ooga booga booga");
-      }
-      //   if (i !== k) {
-      //     console.log("not equal: ", [i, k]);
-      //   } else {
-      //     console.log("equal: ", [i, k]);
-      //   }
-    }
+  function removePairs(arr, a, b) {
+    let indexOne = arr.indexOf(a);
+    let indexTwo = arr.indexOf(b);
+    arr.splice(indexOne, 1);
+    arr.splice(indexTwo, 1);
   }
-  return matches;
+  (function compareNumbers() {
+    for (let i = 0; i < input.length; i++) {
+      for (let k = 0; k < input.length; k++) {
+        if (input[i] === input[k] && i !== k) {
+          removePairs(input, input[i], input[k]);
+        }
+      }
+    }
+  })();
+
+  return input;
 }
 
-console.log(countUnpaired([1, 1, 2, 2, 3, 4]));
+console.log(countUnpaired([7, 1, 1, 2, 3, 5, 5, 5, 5, 5, 1, 2, 7]));
 // console.log([input[i], input[k]]);
 /*
 PLAN A:
@@ -38,5 +32,18 @@ PLAN A:
 PLAN B: 
 1. Loop through input
 2. For every item loop through input again
-3. If the items match but the indices do not, remove both from input
+3. If the items match but the indices do not, remove both from input (HOW?):
+4. Run the loop again. 
+
+
+HOW TO DELETE IT:
+1. 
 */
+
+// if (input[i] === input[k] && i !== k) {
+//     console.log("equal value, different indices: ", [input[i], input[k]]);
+//   } else if (input[i] === input[k] && i === k) {
+//     console.log("equal value, same indices:", [input[i], input[k]]);
+//   } else if (input[i] !== input[k] && i !== k) {
+//     console.log("different value: ", [input[i], input[k]]);
+//   }
